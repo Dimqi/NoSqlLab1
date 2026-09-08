fb715c73257f4b86616ba8ddace20a9b65fecec8
dimqi <dfedorov471@gmail.com>
2026-09-08 15:08:31 +0300

    init commit

---
+++ src/main/java/com/example/nosqllab1/NoSqlLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.nosqllab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSqlLab1Application.class, args);
+    }
+
+}
